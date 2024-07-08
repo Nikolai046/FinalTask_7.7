64055fed44e4161cbb3413687379257e1b6652aa
Nick A <nakursk@gmail.com>
2024-07-08 17:19:57 +0300

    2 фиксция 2й редакции

---
+++ AddressSelector.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask_7
+{
+    public class AddressSelector
+    {
+        private readonly string[,] result;
+        private readonly string type;
+
+        public AddressSelector(string type)
+        {
+            this.type = type;
+            switch (type)
+            {
+                case "PickPoint":
+                    result = PickPointsAddress;
+                    break;
+                case "Shop":
+                    result = ShopAddress;
+                    break;
+                default:
+                    Console.WriteLine($"Неизвестный тип: {type}");
+                    result = new string[0, 0];
+                    break;
+            }
+        }
+
+        public string[] SelectAddress()
+        {
+            int selectedIndex = 0;
+            ConsoleKey key;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Выберите: {GetTypeDescription()}:");
+
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.Write("> ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine($"{result[i, 0]} - {result[i, 1]} - стоимость доставки: {result[i, 2]} руб.");
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.UpArrow && selectedIndex > 0)
+                {
+                    selectedIndex--;
+                }
+                else if (key == ConsoleKey.DownArrow && selectedIndex < result.GetLength(0) - 1)
+                {
+                    selectedIndex++;
+                }
+
+            } while (key != ConsoleKey.Enter);
+
+            return new string[] { result[selectedIndex, 0], result[selectedIndex, 1], result[selectedIndex, 2] };
+        }
+
+        private string GetTypeDescription()
+        {
+            if (type == "PickPoint")
+                return "пункт выдачи и адрес";
+            else if (type == "Shop")
+                return "магазин";
+            // Добавьте здесь описания для будущих типов
+            else
+                return type;
+        }
+
+        private static readonly string[,] ShopAddress = new string[,]
+        {
+        { "Пятёрочка", "Академика Павлова ул., 50", "0.00"},
+        { "Магнит", "Рублёвское ш., 30, корп. 1", "0.00" },
+        {"Перекресток", "Боровая ул., 16", "0.00"},
+        {"Ашан", "Перовская ул., 4, корп. 1","0.00"},
+        { "Карусель", "Бауманская ул., 58А", "0.00"}
+        };
+
+        private static readonly string[,] PickPointsAddress = new string[,]
+        {
+        { "Постамат: 9953", "Маршала Неделина ул., д. 40", "100.00"},
+        { "Постамат: 3856", "Госпитальный Вал ул., д. 8/1", "250.00" },
+        { "Постамат: 1422", "Винокурова ул., д. 22" , "150.00"},
+        { "Постамат: 5656", "3-я Рыбинская ул., д. 1" , "250.00"},
+        { "Постамат: 9544", "Пырьева ул., д. 4, к. 1" , "370.00"}
+        };
+    }
+
+
+    class GetHomeAdress
+    {
+        public void GAdress()
+        {
+            string errorMessage = "";
+            while (errorMessage != null)
+            {
+                string validatedWord = Console.ReadLine().WordValidate(out errorMessage);
+                if (validatedWord != null) Console.WriteLine("Слово валидно: " + validatedWord);
+                else Console.WriteLine(errorMessage);
+            }
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,81 +1,147 @@
-﻿using FinalTask_7;
+﻿using System.Collections.Generic;
 using System;
+using FinalTask_7;
 
-
-namespace FinalTask_7_7
+public abstract class Delivery
 {
-    abstract class Delivery
-    {
-        public string Address { get; set; }
+    protected decimal CalculateCost { get; set; }
+    protected string[] collectData = new AddressSelector("PickPoint").SelectAddress();
 
+    public abstract DeliveryInfo GetDeliveryInfo();
+}
+
+public class DeliveryInfo
+{
+    public string[][] Details { get; set; }
+
+    public DeliveryInfo(int capacity)
+    {
+        Details = new string[capacity][];
+    }
+}
+
+
+public class HomeDelivery : Delivery
+{
+    public string CourierName { get; set; }
+    public  string HomeAdress { get; set; }
+    public DateTime DeliveryTime { get; set; }
+
+    //private string[] collectData = new AddressSelector("PickPoint").SelectAddress();
+    public override DeliveryInfo GetDeliveryInfo()
+    {
+        var info = new DeliveryInfo(4);
+        info.Details[0] = new string[] { "Type", "Home Delivery"};
+        info.Details[1] = new string[] { "CourierName", CourierName};
+        info.Details[2] = new string[] { "Adress", HomeAdress};
+        info.Details[3] = new string[] { "DeliveryTime", DeliveryTime.ToString()};
+        info.Details[4] = new string[] { "CostOfDelivery",  CalculateCost.ToString()};
+        return info;       
+    }
+}
+
+public class PickPointDelivery : Delivery
+{
+    private string PickPointName { get; set; }
+    private string PickPointAdress { get; set; }
+
+    public override DeliveryInfo GetDeliveryInfo()
+    {
+        var info = new DeliveryInfo(3);
+        info.Details[0] = new string[] { "Type", "Pick Point Delivery" };
+        info.Details[1] = new string[] { "PickPointName", collectData[0] };
+        info.Details[2] = new string[] { "Adress", collectData[1] };
+        info.Details[3] = new string[] { "CostOfDelivery", collectData[2] };
+        return info;
+    }
+}
+
+public class ShopDelivery : Delivery
+{
+    public string ShopName { get; set; }
+    public string ShopAdress { get; set; }
+
+    public override DeliveryInfo GetDeliveryInfo()
+    {
+        var info = new DeliveryInfo(3);
+        info.Details[0] = new string[] { "Type", "Shop Delivery" };
+        info.Details[1] = new string[] { "ShopName", collectData[0] };
+        info.Details[2] = new string[] { "Adress", collectData[1] };
+        info.Details[4] = new string[] { "CostOfDelivery", collectData[2] };
+        return info;
+    }
+}
+
+
+
+// Класс для представления продукта
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+}
+
+// Класс для представления контактной информации
+public class ContactInfo
+{
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
+
+// Обобщенный класс заказа
+public class Order<TDelivery> where TDelivery : Delivery
+{
+    public int Number { get; set; }
+    public string Description { get; set; }
+    public TDelivery Delivery { get; set; }
+    public List<Product> Products { get; set; }
+    public ContactInfo CustomerContact { get; set; }
+    public decimal TotalCost { get; private set; }
+}
+
+// Класс для управления коллекцией заказов
+public class OrderCollection<TDelivery> where TDelivery : Delivery
+{
+    private List<Order<TDelivery>> orders;
+
+    public Order<TDelivery> this[int index]
+    {
+        get => orders[index];
+        set => orders[index] = value;
     }
 
-    class HomeDelivery : Delivery
-    {
-       
-    }
+    public void AddOrder(Order<TDelivery> order) { }
+    public void RemoveOrder(int orderNumber) { }
+}
 
-    class PickPointDelivery : Delivery
-    {
-        private readonly string[,] PickPoints = new string[,]
-        {
-        { "Пятёрочка", "Академика Павлова ул., 50" },
-        { "Магнит", "Рублёвское ш., 30, корп. 1"},
-        {"Перекресток", "Боровая ул., 16"},
-        {"Ашан", "Перовская ул., 4, корп. 1"},
-        { "Карусель", "Бауманская ул., 58А"}
-         };
+// Статический класс для работы с заказами
+public static class OrderProcessor
+{
+    public static void ProcessOrder<TDelivery>(Order<TDelivery> order) where TDelivery : Delivery { }
+}
 
-    }
+// Класс для работы с адресами
+public static class AddressHelper
+{
+    public static string FormatAddress(string address) { return address; }
+}
 
-    class ShopDelivery : Delivery
-    {
-        private readonly string[,] PickPoints = new string[,]
-         {
-        { "Постамат: Халва", "Маршала Неделина ул., д. 40" },
-        { "Постамат: Халва", "Госпитальный Вал ул., д. 8/1" },
-        { "Постамат: Халва", "Винокурова ул., д. 22" },
-        { "Постамат: Халва", "3-я Рыбинская ул., д. 1" }, 
-        { "Постамат: Халва", "Пырьева ул., д. 4, к. 1" }
-          };
-    }
-    class GetAdress 
-    {
-        public void GAdress()
-        {
-            string errorMessage = "";
-            while (errorMessage != null)
-            {
-                string validatedWord = Console.ReadLine().WordValidate(out errorMessage);
-                if (validatedWord != null) Console.WriteLine("Слово валидно: " + validatedWord);
-                else Console.WriteLine(errorMessage);
-            }
-        }
+// Расширение для класса Order
+public static class OrderExtensions
+{
+    public static void PrintOrderDetails<TDelivery>(this Order<TDelivery> order) where TDelivery : Delivery { }
+}
 
-    }
-    class Order<TDelivery, TStruct> where TDelivery : Delivery
-    {
-        public TDelivery Delivery;
+// Абстрактный класс для представления компании доставки
+public abstract class DeliveryCompany<TDelivery> where TDelivery : Delivery
+{
+    public string CompanyName { get; set; }
+    public abstract void AssignDelivery(Order<TDelivery> order);
+}
 
-        public int Number;
-
-        public string Description;
-
-        public void DisplayAddress()
-        {
-            Console.WriteLine(Delivery.Address);
-        }
-
-        // ... Другие поля
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            HomeDelivery delivery = new HomeDelivery();
-            delivery.GetAdress();
-
-        }
-    }
+// Класс для представления курьерской компании
+public class CourierCompany : DeliveryCompany<HomeDelivery>
+{
+    public List<string> AvailableCouriers { get; set; }
 }
